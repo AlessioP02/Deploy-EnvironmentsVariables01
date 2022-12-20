d033160bad91b8f0378575eba5bdc2646c2b2d47
AlessioP02 <alessio.alemix41@gmail.com>
2022-12-20 12:25:09 +0100

    Initial commit

---
+++ Deploy-EnvironmentsVariables01/src/main/java/com/example/DeployEnvironmentsVariables01/controllers/BasicController.java
@@ -0,0 +1,20 @@
+package com.example.DeployEnvironmentsVariables01.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BasicController {
+
+    @Autowired
+    private Environment environment;
+
+    @GetMapping("/")
+    public String getVariable(){
+
+        return environment.getProperty("devName") + " " + environment.getProperty("authCode");
+    }
+}
